b79a0c521ceef375c7a4a2f731d2805a9f196893
KienBui <doankien.bui@gmail.com>
2014-02-26 16:18:01 +0700

    clean functions

---
+++ Gemfile
@@ -0,0 +1,30 @@
+source 'https://rubygems.org'
+
+# Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
+gem 'rails', '4.0.2'
+gem 'sqlite3'
+gem 'sass-rails'
+gem 'uglifier', '>= 1.3.0'
+gem 'jquery-rails'
+gem 'turbolinks'
+gem 'jbuilder', '~> 1.2'
+gem 'sqlite3-ruby', :require => 'sqlite3'
+gem 'el_finder'
+gem 'thin'
+
+group :doc do
+  # bundle exec rake doc:rails generates the API under doc/api.
+  gem 'sdoc', require: false
+end
+
+# Use ActiveModel has_secure_password
+# gem 'bcrypt-ruby', '~> 3.1.2'
+
+# Use unicorn as the app server
+# gem 'unicorn'
+
+# Use Capistrano for deployment
+# gem 'capistrano', group: :development
+
+# Use debugger
+# gem 'debugger', group: [:development, :test]
---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,29 @@
+class HomeController < ApplicationController
+
+  def index
+  end
+
+  def elfinder
+    h, r = ElFinder::Connector.new(
+      :root => File.join(Rails.public_path, 'system', 'elfinder'),
+      :url => '/system/elfinder',
+      :perms => {
+        /pjkh\.png$/ => {:write => false, :rm => false},
+        /\.txt$/ => {:write => true,:rm => true}
+      },
+      :extractors => {
+        'application/zip' => ['unzip', '-qq', '-o'],
+        'application/x-gzip' => ['tar', '-xzf'],
+      },
+      :archivers => {
+        'application/zip' => ['.zip', 'zip', '-qr9'],
+        'application/x-gzip' => ['.tgz', 'tar', '-czf'],
+      },
+      :thumbs => true
+    ).run(params)
+
+    headers.merge!(h)
+    render (r.empty? ? {:nothing => true} : {:text => r.to_json}), :layout => false
+  end
+
+end
